6f4a8ec08dedd4b26a28f0719aa7241bd6bd3cc5
Collin Smith <cmdeveloped@gmail.com>
2018-08-14 22:15:18 -0500

    add first sidebar component and start styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
+import './App.min.css';
+import Sidebar from './components/Sidebar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Route exact path="/" render={() => (
+          <main id="home">
+            <div className="body-container">
+
+            </div>
+            <Sidebar />
+          </main>
+        )}/>
+        <Route path="/repos" render={() => (
+          <main id="repos">
+            <div className="body-container">
+
+            </div>
+            <Sidebar />
+          </main>
+        )}/>
       </div>
     );
   }
