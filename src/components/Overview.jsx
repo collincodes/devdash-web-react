3e5596eb84ada21d7097cff152385ee70c2f6366
Collin Smith <cmdeveloped@gmail.com>
2018-08-28 20:39:42 -0500

    add language overview bar

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -15,6 +15,7 @@ class Overview extends Component {
     // track all languages
     let totalBytes = 0
     let js = 0, html = 0, css = 0, python = 0, others = 0
+    let jsColor = '', htmlColor = '', cssColor = '', pythonColor = '', otherColor = ''
     let languages = [] // array to store languages
     for (let repo of repos) {
       if (repo.node.languages.edges.length > 0) { // eslint-disable-next-line
@@ -24,19 +25,24 @@ class Overview extends Component {
           }
           switch (obj.node.name) {
             case 'JavaScript':
+              jsColor = '#001e4b'
               js += obj.size
               break;
             case 'HTML':
+              htmlColor = '#002881'
               html += obj.size
               break;
             case 'CSS':
+              cssColor = '#004db4'
               css += obj.size
               break;
             case 'Python':
+              pythonColor = '#76a0d1'
               python += obj.size
               break;
             default:
-              return others
+              otherColor = '#bebebe'
+              others += obj.size
           }
           totalBytes += obj.size // add to total 100%
         })
@@ -48,32 +54,40 @@ class Overview extends Component {
     const cssPerc = ((css / totalBytes) * 100).toFixed(1)
     const pythonPerc = ((python / totalBytes) * 100).toFixed(1)
 
-    const listLanguages = languages.sort().map((lang, i) => {
-      let percent
-      switch ( lang ) {
-        case 'JavaScript':
-          percent = jsPerc
-          break;
-        case 'HTML':
-          percent = htmlPerc
-          break;
-        case 'CSS':
-          percent = cssPerc
-          break;
-        case 'Python':
-          percent = pythonPerc
-          break;
-        default:
-          percent = ''
+    languages = [
+      {
+        language: 'JavaScript',
+        size: jsPerc,
+        color: jsColor
+      }, {
+        language: 'HTML',
+        size: htmlPerc,
+        color: htmlColor
+      }, {
+        language: 'CSS',
+        size: cssPerc,
+        color: cssColor
+      }, {
+        language: 'Python',
+        size: pythonPerc,
+        color: pythonColor
       }
+    ]
+
+    console.log(others, otherColor)
+
+    const listLanguages = languages.map((lang, i) => {
       return <li key={ i } className="language">
         <div className="stat-bar">
-          <span className="percent">{ percent }%</span>
-          <span className="color"></span>
+          <span className="color" style={{ backgroundColor: lang.color }}></span>
         </div>
-        <span className="label">{ lang }</span>
+        <span className="label">{ lang.language }</span>
       </li>
     });
+
+    const listBars = languages.map((lang, i) => {
+      return <span key={ i } className="bar" title={ lang.size + '%' } style={{ backgroundColor: lang.color, width: 'calc(' + lang.size + '% - 0.25rem' }}></span>
+    })
 
     return (
       <div id="overview" className="card">
@@ -95,6 +109,9 @@ class Overview extends Component {
               <span className="number">{ commitNum }</span>
               <label>Commits</label>
             </div>
+          </div>
+          <div className="language-bar">
+            { listBars }
           </div>
           <div className="more">
             <p className="js">JavaScript Frameworks: React & Ember</p>
